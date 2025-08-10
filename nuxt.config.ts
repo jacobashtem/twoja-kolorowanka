eda45f641a4d9274cd2c200256b3c7b7dbba5406
Jacob Ashtem <jacob.ashtem@gmail.com>
2025-08-10 17:47:07 +0200

    500-bug-netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,85 +1,85 @@
-  import { defineNuxtConfig } from "nuxt/config"
-  import routes from './prerender-routes.json'
+import { defineNuxtConfig } from "nuxt/config"
+import routes from './prerender-routes.json'
 
-  export default defineNuxtConfig({
-    compatibilityDate: '2024-11-01',
-    routeRules: {
-        '/koloruj/**': { ssr: false, prerender: false }
-      },
-      colorMode: {
-      preference: 'light'
-    },
-    devtools: { enabled: true },
-    nitro: {
-      preset: 'static',
-      routeRules: {
-        '/api/**': { prerender: false }
-      },
-      prerender: {
-          routes,            // Twój JSON – zachowaj
-          crawlLinks: true,  // ➜ pozwól Nuxtowi odszukać dodatkowe linki
-          failOnError: false // ➜ nie wywracaj builda na drobnostkach
-        }
-    },
-    content: {
-      documentDriven: false
-    },
-      site: {
-      url: 'https://twoja-kolorowanka.pl',
-      name: 'twoja-kolorowanka.pl',
-      trailingSlash: true
-    },
-     sitemap: {
-        exclude: [/\/\d+\/?$/],
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  routeRules: {
+    '/koloruj/': { ssr: false, prerender: true },
+    '/koloruj/**': { ssr: false, prerender: false },
+    '/api/**': { prerender: false }
   },
-    modules: [
-      '@nuxtjs/sitemap',
-      '@nuxt/content',
-      '@nuxt/ui',
-      '@nuxtjs/tailwindcss',
-      '@vueuse/nuxt',
-      '@nuxtjs/google-fonts',
-      '@zadigetvoltaire/nuxt-gtm',
-    ],
-    googleFonts: {
-      families: {
-        Modak: true,
-      },
-      display: 'swap',
-      preconnect: true,
-      preload: true,
+  colorMode: {
+    preference: 'light'
+  },
+  devtools: { enabled: true },
+  nitro: {
+    preset: 'static',
+    prerender: {
+      routes: [
+        ...routes,
+        '/koloruj/'
+      ],
+      crawlLinks: true,
+      failOnError: false
+    }
+  },
+  content: {
+    documentDriven: false
+  },
+  site: {
+    url: 'https://twoja-kolorowanka.pl',
+    name: 'twoja-kolorowanka.pl',
+    trailingSlash: true
+  },
+  sitemap: {
+    exclude: [/\/\d+\/?$/, '/koloruj/**']
+  },
+  modules: [
+    '@nuxtjs/sitemap',
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxtjs/google-fonts',
+    '@zadigetvoltaire/nuxt-gtm'
+  ],
+  googleFonts: {
+    families: {
+      Modak: true
     },
-    runtimeConfig: {
-      public: {
-        gtm: {
-          id: "GTM-PMTV7XJ8",
-          defer: false,
-          compatibility: false,
-          enabled: true,
-          debug: true,
-          loadScript: true,
-          trackOnNextTick: false,
-          devtools: true,
-        },
-      }
-    },
-    app: {
-      head: {
-        htmlAttrs: {
-          lang: 'pl'
-        },
-        link: [
-          { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-          { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-          { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-          { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-          { rel: 'manifest', href: '/site.webmanifest' },
-          { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
-        ],
-        meta: [
-          { name: 'theme-color', content: '#ffffff' },
-          { name: 'msapplication-TileColor', content: '#ffffff' }
-        ]
+    display: 'swap',
+    preconnect: true,
+    preload: true
+  },
+  runtimeConfig: {
+    public: {
+      gtm: {
+        id: "GTM-PMTV7XJ8",
+        defer: false,
+        compatibility: false,
+        enabled: true,
+        debug: true,
+        loadScript: true,
+        trackOnNextTick: false,
+        devtools: true
       }
     }
-  })
+  },
+  app: {
+    head: {
+      htmlAttrs: { lang: 'pl' },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
+      ],
+      meta: [
+        { name: 'theme-color', content: '#ffffff' },
+        { name: 'msapplication-TileColor', content: '#ffffff' }
+      ]
+    }
+  }
+})
